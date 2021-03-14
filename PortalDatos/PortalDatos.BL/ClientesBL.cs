499c6251f047582d90578dca7ef8c480cd64c343
CELSO FERNANDO REYES MENCIA <cfreyesm@unah.hn>
2021-03-14 16:46:59 -0600

    UpdateOscar

---
+++ PortalDatos/PortalDatos.BL/ClientesBL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDatos.BL
+
+{
+    public class ClientesBL
+    {
+        Contexto _contexto;
+      public List<Cliente> ListasdeClientes { get; set; }
+
+        public ClientesBL()
+        {
+            _contexto = new Contexto();
+            ListasdeClientes = new List<Cliente>();
+        }
+
+        public List<Cliente> ObtenerClientes()
+        {
+            ListasdeClientes = _contexto.Clientes
+                .OrderBy(r => r.Nombre)
+                .ToList();
+
+            return ListasdeClientes;
+        }
+
+        public List<Cliente> ObtenerClientesActivos();
+        {
+               ListadeClientes = _contexto.Clientes
+                .Where(r => r.Activo == true)
+                .OrderBy(r => r.Nombre)
+                .ToList(); 
+
+                return ListasdeClientes;
+            }
+        }
+
+        public void GuardarCliente(Cliente cliente)
+        {
+            if (cliente.Id == 0)
+            {
+                _contexto.Clientes.Add(cliente);
+            }
+            else
+            {
+                var clienteExistente = _contexto.Clientes.Find(cliente.Id);
+
+                clienteExistente.Nombre = cliente.Nombre;
+                clienteExistente.Telefono = cliente.Telefono;
+                clienteExistente.Direccion = cliente.Direccion;
+                clienteExistente.Activo = cliente.Activo;
+            }
+
+            _contexto.SaveChanges();
+        }
+
+        public Cliente ObtenerCliente(int id)
+        {
+            var cliente = _contexto.Clientes.Find(id);
+
+            return cliente;
+        }
+
+        public void EliminarCliente(int id)
+        {
+            var cliente = _contexto.Clientes.Find(id);
+
+            _contexto.Clientes.Remove(cliente);
+            _contexto.SaveChanges();
+        }
+    }
+}
